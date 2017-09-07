f6477af56c8640be5a3b2dfa18100c582944a929
Jerome Kieffer <jerome.kieffer@esrf.fr>
2017-09-07 15:28:28 +0200

    Typos

--- kernel/src/EDObject.py
+++ kernel/src/EDObject.py
@@ -166,7 +166,7 @@ class EDObject(object):
         for name, lst_timimgs in cls.profiling.items():
             npd = numpy.array(lst_timimgs)
             tot = npd.sum()
-            line = " %6d | %8.3f | %8.3 | %9.3 | %s " % \
+            line = " %6d | %8.3f | %8.3f | %9.3f | %s " % \
                     (npd.size, tot / npd.size, npd.std(), tot , name)
             subres[tot] = line
         timimgs = list(subres.keys())
